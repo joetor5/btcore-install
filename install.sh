5fea98ce08baa2062ce5f5190da8392045ee77fe
Joel Torres <joetor5@icloud.com>
2024-07-04 12:47:35 -0500

    Update general install procedures

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ if [ $(uname) == "Darwin" ]; then
 elif [ $(uname) == "Linux" ]; then
     PLATFORM_NAME="linux-gnu"
 else
-    echo "Error: running script on unsupported platform"
+    echo -e "\033[31;1mInstallation aborted: running script on unsupported platform\033[0m"
     exit 1
 fi
 
@@ -37,46 +37,78 @@ function download_bitcoin_core {
 
     for url in $file_download_url $bin_hash_url $hash_sign_url
     do
-        echo "Downloading: $url"
-        curl -s -O --output-dir $VERSION_NUM_FULL $url
+        echo -e "\033[1m==> Downloading $url\033[0m"
+        curl -O --output-dir $VERSION_NUM_FULL $url
     done
 
 }
 
 function verify_bitcoin_core {
 
-    echo "Downloading: builder-keys"
     if [ ! -d $KEYS_REPO ]; then
-        git clone $KEYS_REPO_URL 2> $LOG_FILE
+        echo -e "\033[1m==> Downloading builder-keys ($KEYS_REPO_URL)\033[0m"
+        git clone $KEYS_REPO_URL
     else
-        git -C $KEYS_REPO pull 2> $LOG_FILE
+        echo -e "\033[1m==> Updating builder-keys\033[0m"
+        git -C $KEYS_REPO pull
     fi
     
-    echo "Importing: builder-keys"
-    gpg --import $KEYS_DIR/* 2> $LOG_FILE
-    gpg --keyserver hkps://keys.openpgp.org --refresh-keys 2> $LOG_FILE
+    echo -e "\033[1m==> Importing and refreshing keys\033[0m"
+    gpg --import $KEYS_DIR/*
+    gpg --keyserver hkps://keys.openpgp.org --refresh-keys
 
-    echo "Verifying: hashes and signatures"
+    echo -e "\033[1m==> Verifying hashes and signatures\033[0m"
     cd $VERSION_NUM_FULL
-    shasum -a 256 --ignore-missing --check SHA256SUMS 2> $LOG_FILE
-    gpg --verify SHA256SUMS.asc 2> $LOG_FILE
+    shasum -a 256 --ignore-missing --check SHA256SUMS
+    if [ $? != 0 ]; then
+        echo -e "\033[31;1mInstallation aborted: failure on computing hashes\033[0m"
+        exit 1
+    fi
+
+    touch .hash_verified
+    gpg --verify SHA256SUMS.asc 2> >(grep "Good signature")
+    echo
+    while true; do
+        read -p "The above good signatures were found. Do you trust some of these? [y/n]: " answer
+        case $answer in
+            Y|y)
+                touch .sign_verified; break;;
+            N|n)
+                echo -e "\033[31;1mInstallation aborted: keys not trusted\033[0m"; exit 1;;
+        esac
+    done
+
+    cd ..
 }
 
 function install_bitcoin_core {
-    echo "Installing: $VERSION_NUM_FULL"
+    echo -e "\033[1m==> Installing $VERSION_NUM_FULL\033[0m"
+    cd $VERSION_NUM_FULL
     tar xzf *.tar.gz
 
-    if [ $PLATFORM_NAME == "linux-gnu"]; then
-        sudo install -m 0755 -o root -g root -t /usr/local/bin bin/*
+    if [ $PLATFORM_NAME == "linux-gnu" ]; then
+        sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-$VERSION_NUM/bin/*
     elif [ $PLATFORM_NAME == "apple-darwin" ]; then
         if [ ! -d /usr/local/bin ]; then
             sudo mkdir -p /usr/local/bin
         fi
-        sudo cp bin/bitcoin* /usr/local/bin/.
+        sudo cp bitcoin-$VERSION_NUM/bin/bitcoin* /usr/local/bin/.
     fi
+
+    touch .installed
+
+    cd ..
 }
 
 
-download_bitcoin_core
-verify_bitcoin_core
-install_bitcoin_core
+if [ ! -e $VERSION_NUM_FULL/.hash_verified ]; then
+    download_bitcoin_core
+fi
+
+if [ ! -e $VERSION_NUM_FULL/.sign_verified ]; then
+    verify_bitcoin_core
+fi
+
+if [ ! -e $VERSION_NUM_FULL/.installed ]; then
+    install_bitcoin_core
+fi
