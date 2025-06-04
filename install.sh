f2167826310b4b6b7f65a4ecc737f83cf35ca407
Joel Torres <jt@joeltorres.org>
2025-06-04 18:56:38 -0500

    Add check_latest_btcore_install_version

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 # Distributed under the MIT License. See the accompanying file LICENSE.
 
 BTCORE_INSTALL_NAME="btcore-install"
-BTCORE_INSTALL_URL="https://raw.githubusercontent.com/joetor5/btcore-install/develop/btcore-install.sh"
+BTCORE_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/joetor5/btcore-install/develop/btcore-install.sh"
+BTCORE_INSTALL_VERSION_URL="https://raw.githubusercontent.com/joetor5/btcore-install/develop/.script_version"
 BTCORE_INSTALL_HOME="$HOME/.$BTCORE_INSTALL_NAME"
 BTCORE_INSTALL_BIN="$BTCORE_INSTALL_HOME/bin"
+BTCORE_INSTALL_SCRIPT="$BTCORE_INSTALL_BIN/$BTCORE_INSTALL_NAME"
 
 exit_if_error () {
     if [[ $? != 0 ]]; then
@@ -24,10 +26,22 @@ setup_btcore_install_home () {
 
 }
 
+check_latest_btcore_install_version () {
+
+    if [[ -f $BTCORE_INSTALL_SCRIPT ]]; then
+        latest_version=$(curl -sSL $BTCORE_INSTALL_VERSION_URL)
+        installed_version=$($BTCORE_INSTALL_SCRIPT -v)
+        if [[ $latest_version == $installed_version  ]]; then
+            echo "Latest version ($latest_version) installed, nothing to update."
+            exit 0
+        fi
+    fi
+}
+
 download_btcore_install () {
 
     echo "Downloading and installing $BTCORE_INSTALL_NAME..."
-    curl -sSL -o $BTCORE_INSTALL_NAME --output-dir $BTCORE_INSTALL_BIN $BTCORE_INSTALL_URL && \
+    curl -sSL -o $BTCORE_INSTALL_NAME --output-dir $BTCORE_INSTALL_BIN $BTCORE_INSTALL_SCRIPT_URL && \
     chmod +x $BTCORE_INSTALL_BIN/$BTCORE_INSTALL_NAME
     exit_if_error "unable to install $BTCORE_INSTALL_NAME"
     
@@ -37,6 +51,7 @@ download_btcore_install () {
 
 install () {
     setup_btcore_install_home
+    check_latest_btcore_install_version
     download_btcore_install
 }
 
